2a0bb7135f1b0e0e60e7451a9915c370c1b4e21f
maypop-the-dragon <blainephargett@gmail.com>
2024-01-07 22:27:23 -0600

    resize /pics/ds-day.svg
    The navigation is now correct, but the banner is still sometimes the wrong size. It appears that the browser is not respecting the width and height attributes when the page first loads... or something? Anyway, I resized it with an online SVG editor to exactly 300 pixels wide, which is significantly less than the 3DS's 320px bottom screen width. Hopefully this fixes it!

--- ds-day/common.js
+++ ds-day/common.js
@@ -6,6 +6,7 @@ var IS_3DS = navigator.userAgent.indexOf("Nintendo 3DS") != -1 || (screen.pixelD
 
 if (IS_3DS) {
 	document.addEventListener("load", function(event) {
+		alert("You're on a 3DS.");
 		// Put the header on the top screen.
 		this.body.scrollTo(0, 215);
 	});
