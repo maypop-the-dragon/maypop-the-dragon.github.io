fd66cebe5b36685ef6c969faa6fdcd3388c87979
maypop-the-dragon <blainephargett@gmail.com>
2024-01-07 23:07:44 -0600

    do you see that -> . <- that's my remaining sanity
    (By the way, I thought of a small optimization for /x/eval. This time, I am attempting to wrap the logo in an <svg> tag and take a more direct approach to scrolling the page down as well.

--- ds-day/common.js
+++ ds-day/common.js
@@ -2,17 +2,9 @@
 
 // DS Day Official Site Common JS
 
-alert("Script was run");
-
 var IS_3DS = navigator.userAgent.indexOf("Nintendo 3DS") != -1 || (screen.pixelDepth == 16 && navigator.platform == "iPhone");
 
-alert("IS_3DS was set");
-
 if (IS_3DS) {
-	alert("IS_3DS is true");
-	document.addEventListener("load", function(event) {
-		alert("load listener was executed");
-		// Put the header on the top screen.
-		window.scrollTo(0, 215);
-	});
+	// Put the header on the top screen.
+	window.scrollTo(0, 215);
 }
