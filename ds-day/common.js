aa71fd8e335490274b79506377f0694f653efdde
maypop-the-dragon <blainephargett@gmail.com>
2024-01-07 23:40:35 -0600

    use closing slashes for single tags
    I forgot what they're called. Uh, tags like <br> and <img> that don't need a closing tag. I'm including the <slash /> at the end like HTML-4. Just for, uh, maximum backwards compatibility. Now, I've added an SVG source. Let's hope 3DS does not support <picture>.

--- ds-day/common.js
+++ ds-day/common.js
@@ -5,6 +5,5 @@
 var IS_3DS = navigator.userAgent.indexOf("Nintendo 3DS") != -1 || (screen.pixelDepth == 16 && navigator.platform == "iPhone");
 
 if (IS_3DS) {
-	// Put the header on the top screen.
-	window.scrollTo(0, 215);
+	// TODO: Do something fun.
 }
