c20d5a219c7b7f81a823d60f5b82c0db0febe307
maypop-the-dragon <blainephargett@gmail.com>
2024-01-07 22:45:04 -0600

    i'm getting desperate
    I am resorting to !important.

--- ds-day/common.js
+++ ds-day/common.js
@@ -2,12 +2,17 @@
 
 // DS Day Official Site Common JS
 
+alert("Script was run");
+
 var IS_3DS = navigator.userAgent.indexOf("Nintendo 3DS") != -1 || (screen.pixelDepth == 16 && navigator.platform == "iPhone");
 
+alert("IS_3DS was set");
+
 if (IS_3DS) {
+	alert("IS_3DS is true");
 	document.addEventListener("load", function(event) {
-		alert("You're on a 3DS.");
+		alert("load listener was executed");
 		// Put the header on the top screen.
-		this.body.scrollTo(0, 215);
+		window.scrollTo(0, 215);
 	});
 }
